99dd55aec8ad61d3c8ce8c834053db488f96faee
Jonatan Larsson <jonatanlarsson2001@gmail.com>
2019-08-27 04:00:09 +1100

    Adding output

--- src/gitannoyance/GitAnnoyance.java
+++ src/gitannoyance/GitAnnoyance.java
@@ -15,6 +15,7 @@ public class GitAnnoyance {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("I wonder if this will actually work");
     }
 
 }
